6ce9b96d2c2c62843559fd00f9b923f39be2b670
JiaYu <alice49885@gmail.com>
2024-08-08 09:51:15 +0800

    手機頂列變色

---
+++ index.js
@@ -0,0 +1,20 @@
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+const metaThemeColor = document.querySelector("meta[name='theme-color']");
+
+handleHeaderLight()
+
+window.addEventListener("scroll", () => handleHeaderLight())
+function handleHeaderLight() {
+	const scrollTop = window.scrollY;
+	let themeColor = '#ad0000';
+	if (scrollTop > 0) {
+		header.classList.add('light');
+		themeColor = '#6495ed';
+	} else {
+		header.classList.remove('light');
+	}
+	metaThemeColor.setAttribute('content', themeColor);
+	body.style.backgroundColor = themeColor;
+}
