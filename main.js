5e41518b8d03c702eb7cfbbdc6fe5c745779815e
JiaYu <alice49885@gmail.com>
2024-08-08 10:26:15 +0800

    改回js原生寫法

--- main.js
+++ main.js
@@ -5,36 +5,36 @@ $(document).ready(function() {
     console.log("jQuery is loaded and ready!");
 });
 
-// const header = document.querySelector("header");
-// const nav = document.querySelector("nav");
-// const body = document.querySelector("body");
-// const metaThemeColor = document.querySelector("meta[name='theme-color']");
+const header = document.querySelector("header");
+const nav = document.querySelector("nav");
+const body = document.querySelector("body");
+const metaThemeColor = document.querySelector("meta[name='theme-color']");
 
 handleHeaderLight();
 
 window.addEventListener("scroll", () => handleHeaderLight());
-// function handleHeaderLight() {
-// 	const scrollTop = window.scrollY;
-// 	let themeColor = '#ad0000';
-// 	if (scrollTop > 0) {
-// 		header.classList.add('light');
-// 		themeColor = '#6495ed';
-// 	} else {
-// 		header.classList.remove('light');
-// 	}
-// 	metaThemeColor.setAttribute('content', themeColor);
-// 	body.style.backgroundColor = themeColor;
-// }
-
 function handleHeaderLight() {
 	const scrollTop = window.scrollY;
 	let themeColor = '#ad0000';
 	if (scrollTop > 0) {
-		$('header').addClass('light');
-		themeColor = '#ffffff';
+		header.classList.add('light');
+		themeColor = '#6495ed';
 	} else {
-		$('header').removeClass('light');
+		header.classList.remove('light');
 	}
-	$('meta[name="theme-color"]').attr('content',themeColor);
-	$('body').css('background-color', themeColor);
+	metaThemeColor.setAttribute('content', themeColor);
+	body.style.backgroundColor = themeColor;
 }
+
+// function handleHeaderLight() {
+// 	const scrollTop = window.scrollY;
+// 	let themeColor = '#ad0000';
+// 	if (scrollTop > 0) {
+// 		$('header').addClass('light');
+// 		themeColor = '#ffffff';
+// 	} else {
+// 		$('header').removeClass('light');
+// 	}
+// 	$('meta[name="theme-color"]').attr('content',themeColor);
+// 	$('body').css('background-color', themeColor);
+// }
