c47832f57e1e4bd96033d0a5e7cb31bb50c811d2
JiaYu <alice49885@gmail.com>
2024-08-08 10:18:43 +0800

    加入 jquery

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 import './assets/scss/all.scss';
+import $ from 'jquery';
 
-const header = document.querySelector("header");
-const nav = document.querySelector("nav");
-const body = document.querySelector("body");
-const metaThemeColor = document.querySelector("meta[name='theme-color']");
+$(document).ready(function() {
+    console.log("jQuery is loaded and ready!");
+});
 
-handleHeaderLight()
+// const header = document.querySelector("header");
+// const nav = document.querySelector("nav");
+// const body = document.querySelector("body");
+// const metaThemeColor = document.querySelector("meta[name='theme-color']");
 
-window.addEventListener("scroll", () => handleHeaderLight())
+handleHeaderLight();
+
+window.addEventListener("scroll", () => handleHeaderLight());
+// function handleHeaderLight() {
+// 	const scrollTop = window.scrollY;
+// 	let themeColor = '#ad0000';
+// 	if (scrollTop > 0) {
+// 		header.classList.add('light');
+// 		themeColor = '#6495ed';
+// 	} else {
+// 		header.classList.remove('light');
+// 	}
+// 	metaThemeColor.setAttribute('content', themeColor);
+// 	body.style.backgroundColor = themeColor;
+// }
+
 function handleHeaderLight() {
 	const scrollTop = window.scrollY;
 	let themeColor = '#ad0000';
 	if (scrollTop > 0) {
-		header.classList.add('light');
-		themeColor = '#6495ed';
+		$('header').addClass('light');
+		themeColor = '#ffffff';
 	} else {
-		header.classList.remove('light');
+		$('header').removeClass('light');
 	}
-	metaThemeColor.setAttribute('content', themeColor);
-	body.style.backgroundColor = themeColor;
+	$('meta[name="theme-color"]').attr('content',themeColor);
+	$('body').css('background-color', themeColor);
 }
